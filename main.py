182aa267654d60329fc29a9a691b920b927bb834
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2025-10-01 20:39:56 -0500

    Creating query parameters to test retrieving data from API

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ def retrieve_key():
     """Retrieves API key"""
     key = os.environ.get('SPOONACULAR_KEY')
     return key
+
+def retrieve_recipe_ID(key_input, name_input):
+    """Retrieves recipe ID from API based on recipe name provided by AI"""
+    url = 'https://api.spoonacular.com/recipes/complexSearch?'
+    query = {'titleMatch': name_input, 'apiKey': key_input}
    
 def main():
     retrieve_key()
