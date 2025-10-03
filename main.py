016007caada4579817162e88aa9b2c05ef290e85
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2025-10-02 19:52:26 -0500

    Completed retrieve_recipe_id, only retrieves raw json to test what results are returned from input strings

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import os
-
+from pprint import pprint
 
 def retrieve_key():
     """Retrieves API key"""
@@ -11,8 +11,13 @@ def retrieve_recipe_ID(key_input, name_input):
     """Retrieves recipe ID from API based on recipe name provided by AI"""
     url = 'https://api.spoonacular.com/recipes/complexSearch?'
     query = {'titleMatch': name_input, 'apiKey': key_input}
+
+    data = requests.get(url, params=query).json()
+    return data
    
 def main():
-    retrieve_key()
+    key = retrieve_key()
+    food_id = retrieve_recipe_ID(key, 'vanilla cake')
+    pprint(food_id)
 
 main()
