9f39e5344f408c7f985e33dd3cbd2fd4631e4651
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2025-10-09 13:42:45 -0500

    Trying to fix bug to reference query_api in api_logic, keep getting error saying location not found

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from api_logic import query_api as api
 
 
 
+
 def main():
     key = api.retrieve_key()
-    recipe_all = api.query_api_ids(key, 'vanilla cake')
+    recipe_all = api.query_api_ids(key, 'Burrito')
     all_id = api.append_recipe_id(recipe_all)
     api.retrieve_recipes(key, all_id)
 
-main()
+
+if __name__ == '__main__':
+    main()
