905e3574b629db0319a2f82d090c53743b983cfc
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2025-10-20 11:48:38 -0500

    Changed returned variables and arguments to better match output/input of functions

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from api_logic import query_api as api
 def main():
     key = api.retrieve_key()
     recipe_all, recipe_name = api.query_api(key, 'Spaghetti')
-    all_id = api.parse_api_return(recipe_all, recipe_name)
-    api.retrieve_recipes(key, all_id)
+    chosen_id = api.parse_api_return(recipe_all, recipe_name)
+    api.retrieve_recipes(key, chosen_id)
 
 
 if __name__ == '__main__':
