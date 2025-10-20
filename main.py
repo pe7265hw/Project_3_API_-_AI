9fb67ed080adf3de22de8c85316df0abacc40ce2
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2025-10-20 12:11:17 -0500

    Tested and working as expected, last commit & push before PR

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from api_logic import query_api as api
 
 def main():
     key = api.retrieve_key()
-    recipe_all, recipe_name = api.query_api(key, 'Spaghetti')
+    recipe_all, recipe_name = api.query_api(key, 'tortellini')
     chosen_id = api.parse_api_return(recipe_all, recipe_name)
     api.retrieve_recipes(key, chosen_id)
 
