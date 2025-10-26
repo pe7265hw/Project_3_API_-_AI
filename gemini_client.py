c458b5266c1682d45fb30cfbdde5eee3c6b4b837
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2025-10-26 10:02:45 -0500

    In gemini_client.py moved system_prompt into fucntion that can be called by gemini_recipe_chat, removed a few lines for partner to review for removal, realized we needed them

--- gemini_client.py
+++ gemini_client.py
@@ -10,24 +10,24 @@ logging.basicConfig(level=logging.INFO)
 # import os
 # os.environ["GEMINI_API_KEY"] = "YOUR_API_KEY"
 
-system_prompt = """
-You are a friendly and helpful recipe assistant. Your goal is to provide users with recipes that match their desired cuisine and healthiness level as well as their expense choice.
-Follow these steps precisely:
-1.  The user will provide you with a cuisine type (e.g., Italian, Mexican, Indian) and one of three preferences for healthiness (e.g., unhealthy, somewhat healthy, or healthy) and expense (e.g., cheap, moderate, or expensive).
-2.  Based on the cuisine and preferences provided by the user, suggest 3 recipe names that fits those criteria.
-3.  You will return your responce in the exact same format as the response schema provided, with no additional text or explanation.
-Here is the response schema you must follow:
-{
-  [
-    "Recipe Name 1",
-    "Recipe Name 2",
-    "Recipe Name 3"
-  ]
-}
-"""
-cuisine = "Italian"
-diet = "healthy"
-expense = "expensive"
+def system_prompt_info():
+
+    system_prompt = """
+    You are a friendly and helpful recipe assistant. Your goal is to provide users with recipes that match their desired cuisine and healthiness level as well as their expense choice.
+    Follow these steps precisely:
+    1.  The user will provide you with a cuisine type (e.g., Italian, Mexican, Indian) and one of three preferences for healthiness (e.g., unhealthy, somewhat healthy, or healthy) and expense (e.g., cheap, moderate, or expensive).
+    2.  Based on the cuisine and preferences provided by the user, suggest 3 recipe names that fits those criteria.
+    3.  You will return your responce in the exact same format as the response schema provided, with no additional text or explanation.
+    Here is the response schema you must follow:
+    {
+    [
+        "Recipe Name 1",
+        "Recipe Name 2",
+        "Recipe Name 3"
+    ]
+    }
+    """
+    return system_prompt
 
 ##Lines that are double commented out are for team partner to review removing, I believe we do not need them
 
@@ -40,13 +40,14 @@ def gemini_recipe_chat(cuisine, diet, expense):
     """This does not create an actual chat, instead it uses 2 separate GenerateContentConfig calls with different configurations 
     to allow for the response schema to be applied only on the 2nd turn."""
 
+    system_prompt = system_prompt_info()
     
-    ##formatted_user_input = f"Give me some {cuisine} recipes that are {diet}, make sure the price is {expense}."
+    formatted_user_input = (f"Give me some {cuisine} recipes that are {diet}, make sure the price is {expense}.")
     try:
         client = genai.Client()
         response = client.models.generate_content(
             model="gemini-2.5-flash",
-            ##contents=formatted_user_input,
+            contents=formatted_user_input,
             config=GenerateContentConfig(
                 system_instruction=system_prompt,
                 response_mime_type="application/json",
